544e65ba4e4ee20e37346eaf27e5816fce10c08a
Naomi Cole <naomisarahcole@gmail.com>
2022-08-02 14:56:14 +1000

    refactored all into correct components

--- src/index.js
+++ src/index.js
@@ -1,104 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
-
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
-} 
-
-const linkStyles = {
-  width: "100px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-};
-
-function Navbar() {
-  return (
-    <div>
-      <NavLink
-        to="/"
-        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
-        exact
-        /* add styling to Navlink */
-        style={linkStyles}
-        /* add prop for activeStyle */
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/login"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Login
-      </NavLink>
-    </div>
-  );
-}
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
 
 ReactDOM.render(
   <Router>
-    <Navbar/>
-    <Switch>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route exact path="/">
-        <Home />
-      </Route> 
-    </Switch>
+    <App />
   </Router>,
   document.getElementById("root")
 ); 
